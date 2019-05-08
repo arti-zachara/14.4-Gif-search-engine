aa8328b758f9d1acf629ae3cdb9f02c2e4fbb07c
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-05-08 13:13:53 +0200

    Change App.js: promise and ES6 syntax

--- js/components/App.js
+++ js/components/App.js
@@ -1,6 +1,6 @@
 // API variables:
-var GIPHY_API_URL = "https://api.giphy.com";
-var GIPHY_PUB_KEY = "LJvJkg6M86Lh1D68H5qJg7JvXrjsajTa";
+const GIPHY_API_URL = "https://api.giphy.com";
+const GIPHY_PUB_KEY = "LJvJkg6M86Lh1D68H5qJg7JvXrjsajTa";
 
 App = React.createClass({
   // initial state
@@ -17,45 +17,51 @@ App = React.createClass({
     this.setState({
       loading: true
     });
-    this.getGif(
-      searchingText,
-      function(gif) {
-        // dziwi mnie takie wywołanie
+    this.getGif(searchingText)
+      .then(response =>
         this.setState({
           // stop loading - loading state
           loading: false,
-          gif: gif,
+          gif: response,
           searchingText: searchingText
-        });
-      }.bind(this)
-    );
+        })
+      )
+      .catch(error => console.error("Error has occurred", error));
   },
+
   // get gif with parameter: typed text and a callback function that will return gif
-  getGif: function(searchingText, callback) {
-    var url =
-      GIPHY_API_URL +
-      "/v1/gifs/random?api_key=" +
-      GIPHY_PUB_KEY +
-      "&tag=" +
-      searchingText;
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        var data = JSON.parse(xhr.responseText).data;
-        var gif = {
-          url: data.fixed_width_downsampled_url,
-          sourceUrl: data.url
-        };
-        // callback function with gif parameters
-        callback(gif);
-      }
-    };
-    xhr.send();
+  getGif: function(searchingText) {
+    // promise
+    return new Promise(function(resolve, reject) {
+      const url =
+        GIPHY_API_URL +
+        "/v1/gifs/random?api_key=" +
+        GIPHY_PUB_KEY +
+        "&tag=" +
+        searchingText;
+      const xhr = new XMLHttpRequest();
+
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          const data = JSON.parse(xhr.responseText).data;
+          const gif = {
+            url: data.fixed_width_downsampled_url,
+            sourceUrl: data.url
+          };
+          // no errors -> resolve
+          resolve(gif);
+        } else {
+          // errors - > reject with reason
+          reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+        }
+      };
+      xhr.open("GET", url);
+      xhr.send();
+    });
   },
 
   render: function() {
-    var styles = {
+    const styles = {
       margin: "0 auto",
       textAlign: "center",
       width: "90%"
